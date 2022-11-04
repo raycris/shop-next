7ea48b3372796e974b4679232491e06e5c85a2b7
Raycris Maldonado <raycris.maldonado@utreee.com>
2022-11-04 15:55:20 -0400

    extending app document

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+
+/* En react 18 children fue eliminado del tipo FC, 
+para que typescript no de error debes de añadirlo por cuenta propia */
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout: React.FC<Props> = ({children}) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+      <footer>Footer</footer>
+    </div>
+  );
+};
+
+export default Layout;
